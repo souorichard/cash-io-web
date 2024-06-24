f7d8ea7611d144c2d56367929b8b6ca1879cf946
souorichard <elaine.ra96@gmail.com>
2024-06-24 20:56:51 -0300

    refactor: add auth banner

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,3 +1,4 @@
+import { Logo, LogoEmblem, LogoName } from '@/components/application/logo'
 import { AuthPage } from '@/components/authentication/page'
 import { PropsWithChildren } from 'react'
 
@@ -5,7 +6,22 @@ export default function AuthLayout({ children }: PropsWithChildren) {
   return (
     <main className="min-h-screen grid grid-cols-2">
       <AuthPage>{children}</AuthPage>
-      <section className="bg-zinc-200"></section>
+      <section className="p-10 flex flex-col bg-auth bg-cover bg-bottom">
+        <Logo>
+          <LogoEmblem className="fill-white" />
+          <LogoName className="text-white" />
+        </Logo>
+
+        <div className="mt-auto flex justify-between items-center text-sm text-white">
+          <span>
+            Painel do parceiro &copy; Cash.io - {new Date().getFullYear()}
+          </span>
+
+          <span>
+            Designed by <b>Freepik</b>
+          </span>
+        </div>
+      </section>
     </main>
   )
 }
