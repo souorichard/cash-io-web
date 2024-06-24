b01760b5e7a4af1b0754f56cf2d5660d2d7b7232
souorichard <elaine.ra96@gmail.com>
2024-06-24 19:29:25 -0300

    feat: add error-label component

---
+++ src/components/application/error-label.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react'
+import { cn } from '@/lib/utils'
+
+interface ErrorLabelProps {
+  className?: string
+  children: ReactNode
+}
+
+export default function ErrorLabel({ children, className }: ErrorLabelProps) {
+  return (
+    <span className={cn('text-sm text-red-700 dark:text-red-500', className)}>
+      {children}
+    </span>
+  )
+}
