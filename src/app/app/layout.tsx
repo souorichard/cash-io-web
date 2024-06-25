092270396b6e2e42687d0031d4189a14aedf61fd
souorichard <elaine.ra96@gmail.com>
2024-06-24 21:24:07 -0300

    feat: add application layout

---
+++ src/app/app/layout.tsx
@@ -0,0 +1,11 @@
+import { PropsWithChildren } from 'react'
+import { MainHeader } from '@/components/application/main-header'
+
+export default function AppLayout({ children }: PropsWithChildren) {
+  return (
+    <div className="min-h-screen grid grid-rows-[auto_1fr]">
+      <MainHeader />
+      {children}
+    </div>
+  )
+}
