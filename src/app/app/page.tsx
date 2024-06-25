d9dc5e8db6750cb59c45f4abae13ef17278641c3
souorichard <elaine.ra96@gmail.com>
2024-06-25 20:47:39 -0300

    feat: add transactions-in-period-card component

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -7,6 +7,7 @@ import {
 import { ExpenseTransactionsCard } from '@/components/application/expense-transactions-card'
 import { RevenueTransactionsCard } from '@/components/application/revenue-transactions-card'
 import { TotalBalanceCard } from '@/components/application/total-balance-card'
+import { TransactionsInPeriodCard } from '@/components/application/transactions-in-period-card'
 
 export default function DashboardPage() {
   return (
@@ -15,10 +16,11 @@ export default function DashboardPage() {
         <ApplicationPageTitle>Dashboard</ApplicationPageTitle>
       </ApplicationPageHeader>
       <ApplicationPageContent>
-        <div className="grid grid-cols-3 gap-x-4">
-          <ExpenseTransactionsCard />
-          <RevenueTransactionsCard />
-          <TotalBalanceCard />
+        <div className="grid grid-cols-3 gap-4">
+          <ExpenseTransactionsCard className="col-span-1" />
+          <RevenueTransactionsCard className="col-span-1" />
+          <TotalBalanceCard className="col-span-1" />
+          <TransactionsInPeriodCard className="col-span-2" />
         </div>
       </ApplicationPageContent>
     </ApplicationPage>
---
+++ src/components/application/transactions-in-period-card.tsx
@@ -0,0 +1,128 @@
+'use client'
+
+import { cn } from '@/lib/utils'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '../ui/card'
+import { Label } from '../ui/label'
+import { DatePickerWithRange } from './date-picker-with-range'
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  TooltipProps,
+  XAxis,
+  YAxis,
+} from 'recharts'
+import { violet } from 'tailwindcss/colors'
+
+const data = [
+  {
+    date: '01/02/2024',
+    receipt: 1000,
+  },
+  {
+    date: '02/02/2024',
+    receipt: 100,
+  },
+  {
+    date: '03/02/2024',
+    receipt: 600,
+  },
+  {
+    date: '04/02/2024',
+    receipt: 300,
+  },
+]
+
+function CustomTooltip({
+  active,
+  payload,
+  label,
+}: TooltipProps<number, number>) {
+  if (active && payload && payload.length) {
+    return (
+      <div className="flex gap-1 rounded-l border bg-card p-2 text-sm text-card-foreground shadow-sm">
+        <span className="font-semibold">{label}</span>
+        <span>-</span>
+        <span>
+          {payload[0].value?.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          })}
+        </span>
+      </div>
+    )
+  }
+
+  return null
+}
+
+interface TransactionsInPeriodCardProps {
+  className?: string
+}
+
+export function TransactionsInPeriodCard({
+  className,
+}: TransactionsInPeriodCardProps) {
+  return (
+    <Card className={cn('', className)}>
+      <CardHeader className="flex-row items-center">
+        <div className="space-y-1.5">
+          <CardTitle>Transações no Período</CardTitle>
+          <CardDescription>
+            Veja a receita diária no período selecionado
+          </CardDescription>
+        </div>
+
+        <div className="ml-auto flex items-center gap-3">
+          <Label>Período</Label>
+          <DatePickerWithRange />
+        </div>
+      </CardHeader>
+      <CardContent>
+        <ResponsiveContainer width="100%" height={280}>
+          <LineChart data={data} style={{ fontSize: 12 }}>
+            <XAxis
+              dataKey="date"
+              stroke="#888888"
+              tickLine={false}
+              axisLine={false}
+              dy={16}
+            />
+
+            <YAxis
+              stroke="#888888"
+              tickLine={false}
+              axisLine={false}
+              width={80}
+              tickFormatter={(value: number) =>
+                value.toLocaleString('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                })
+              }
+            />
+
+            <CartesianGrid className="!stroke-muted" vertical={false} />
+
+            <Line
+              type="linear"
+              strokeWidth={2}
+              dataKey="receipt"
+              stroke={violet['500']}
+            />
+
+            <Tooltip cursor={false} content={<CustomTooltip />} />
+          </LineChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  )
+}
