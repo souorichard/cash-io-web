20468f972a118e41a5b2ce6f2d56444e82983f63
souorichard <elaine.ra96@gmail.com>
2024-06-26 20:14:29 -0300

    feat: add transactions page

---
+++ src/app/app/transactions/page.tsx
@@ -0,0 +1,17 @@
+import {
+  ApplicationPage,
+  ApplicationPageContent,
+  ApplicationPageHeader,
+  ApplicationPageTitle,
+} from '@/components/application/patterns/page'
+
+export default function TransactionsPage() {
+  return (
+    <ApplicationPage>
+      <ApplicationPageHeader>
+        <ApplicationPageTitle>Transações</ApplicationPageTitle>
+      </ApplicationPageHeader>
+      <ApplicationPageContent>test</ApplicationPageContent>
+    </ApplicationPage>
+  )
+}
