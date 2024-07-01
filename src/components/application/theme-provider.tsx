b94331f2b671dd5b8553cb772e2841085e226c50
souorichard <elaine.ra96@gmail.com>
2024-07-01 12:24:36 -0300

    feat: implementing axios and react-query

--- src/components/application/theme-provider.tsx
+++ src/components/application/theme-provider.tsx
@@ -3,7 +3,13 @@
 import * as React from 'react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { type ThemeProviderProps } from 'next-themes/dist/types'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '@/lib/react-query'
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <NextThemesProvider {...props}>{children}</NextThemesProvider>
+    </QueryClientProvider>
+  )
 }
