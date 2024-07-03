cd92dd02afe7637ef2c371625329acae3c45935b
souorichard <elaine.ra96@gmail.com>
2024-07-03 09:23:24 -0300

    feat: add settings layout.tsx

---
+++ src/app/app/settings/layout.tsx
@@ -0,0 +1,29 @@
+import { Metadata } from 'next'
+import { PropsWithChildren } from 'react'
+import {
+  ApplicationPage,
+  ApplicationPageContent,
+  ApplicationPageHeader,
+  ApplicationPageTitle,
+} from '@/components/application/patterns/page'
+import { SettingsSidebar } from '@/components/application/settings-sidebar'
+
+export const metadata: Metadata = {
+  title: 'Configurações',
+}
+
+export default function SettingsLayout({ children }: PropsWithChildren) {
+  return (
+    <ApplicationPage>
+      <ApplicationPageHeader>
+        <ApplicationPageTitle>Configurações</ApplicationPageTitle>
+      </ApplicationPageHeader>
+      <ApplicationPageContent>
+        <div className="grid grid-cols-[16rem_1fr] gap-10">
+          <SettingsSidebar />
+          {children}
+        </div>
+      </ApplicationPageContent>
+    </ApplicationPage>
+  )
+}
