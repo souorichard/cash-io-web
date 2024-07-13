772066808ce471435d215a78218bac43f9b6e806
souorichard <elaine.ra96@gmail.com>
2024-07-13 15:00:43 -0300

    refactor: update and implement total-balance api route

--- src/api/analytics/get-total-balance.ts
+++ src/api/analytics/get-total-balance.ts
@@ -2,12 +2,19 @@ import Cookies from 'js-cookie'
 
 import { api } from '@/lib/axios'
 
+export interface TotalBalanceResponse {
+  balance: number
+}
+
 export async function getTotalBalance() {
-  const response = await api.get<number>('/transactions/total-balance', {
-    headers: {
-      Authorization: `Bearer ${Cookies.get('token')}`,
+  const response = await api.get<TotalBalanceResponse>(
+    `/team/${Cookies.get('teamId')}/transactions/balance`,
+    {
+      headers: {
+        Authorization: `Bearer ${Cookies.get('token')}`,
+      },
     },
-  })
+  )
 
   return response.data
 }
--- src/components/application/total-balance-card.tsx
+++ src/components/application/total-balance-card.tsx
@@ -20,6 +20,8 @@ export function TotalBalanceCard({ className }: TotalBalanceCardProps) {
     refetchOnWindowFocus: false,
   })
 
+  const balance = totalBalance?.balance ? totalBalance?.balance : 0
+
   return (
     <Card className={cn('bg-card-foreground', className)}>
       <CardHeader className="pb-3 flex-row items-center">
@@ -36,7 +38,7 @@ export function TotalBalanceCard({ className }: TotalBalanceCardProps) {
         ) : (
           <>
             <b className="text-2xl font-bold text-primary-foreground">
-              {totalBalance
+              {balance
                 ?.toLocaleString('pt-BR', {
                   style: 'currency',
                   currency: 'BRL',
@@ -44,8 +46,7 @@ export function TotalBalanceCard({ className }: TotalBalanceCardProps) {
                 .replace('-', '- ')}
             </b>
             <p className="mt-1 text-xs text-zinc-400 dark:text-zinc-600">
-              <span className="text-emerald-600">+20%</span> em relação ao mês
-              passado
+              A cada 30 dias compare seu saldo
             </p>
           </>
         )}
