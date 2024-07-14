c497d13dbd9209b81ffdc2923a6f16ce1dcd6d59
souorichard <elaine.ra96@gmail.com>
2024-07-13 22:12:34 -0300

    refactor: update delete-transaction api route

--- src/api/transaction/delete-transaction.ts
+++ src/api/transaction/delete-transaction.ts
@@ -3,11 +3,14 @@ import Cookies from 'js-cookie'
 import { api } from '@/lib/axios'
 
 export async function deleteTransaction(id: string) {
-  const response = await api.delete(`/transactions/${id}`, {
-    headers: {
-      Authorization: `Bearer ${Cookies.get('token')}`,
+  const response = await api.delete(
+    `/team/${Cookies.get('teamId')}/transactions/${id}`,
+    {
+      headers: {
+        Authorization: `Bearer ${Cookies.get('token')}`,
+      },
     },
-  })
+  )
 
   return response.data
 }
